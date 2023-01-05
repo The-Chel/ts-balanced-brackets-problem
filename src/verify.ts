4b90325f609b7313a3570f70f683b95425f2ae3d
Dmitriy Kuzmin <kuzmin_dmitrii@inbox.ru>
2023-01-05 11:47:29 +0400

    temp solving

--- src/verify.ts
+++ src/verify.ts
@@ -1,3 +1,52 @@
 export function verify(str: string): boolean {
-
+  const strIn = str;
+  // const nested = [];
+  let counter = true;
+  for (let i = 0; i < strIn.length; i++) {
+    
+    if (strIn[i]==='(') {
+      for (let k = i; k < strIn.length; k++) {
+        if (strIn[k]===')') {
+          counter = true;
+          break;
+        }  else {
+          counter = false;
+        }      
+      }
+    }
+    if (strIn[i]==='{') {
+      for (let k = i; k < strIn.length; k++) {
+        if (strIn[k]==='}') {
+          counter = true;
+          break;
+        }  else {
+          counter = false;
+        }      
+      }
+    }
+    if (strIn[i]==='[') {
+      for (let k = i; k < strIn.length; k++) {
+        if (strIn[k]===']') {
+          counter = true;
+          break;
+        }  else {
+          counter = false;
+        }      
+      }
+    }
+    if (strIn[i]==='<') {
+      for (let k = i; k < strIn.length; k++) {
+        if (strIn[k]==='>') {
+          counter = true;
+          break;
+        }  else {
+          counter = false;
+        } 
+      }
+    }
+  }
+  // console.log(counter);
+  
+  return counter;
 }
+// verify('----(+++++)-----');
